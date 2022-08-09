51e5a30895e58a11a7e3c569b20db8b3ef73bae0
cristiano <originalvgo@gmail.com>
2022-08-09 14:20:21 -0300

    block class

--- block.js
+++ block.js
@@ -1,13 +1,13 @@
 class Block {
 
-    constructor(timestamp, lasthash, hash, data){
+    constructor(timestamp, lasthash, hash, data) {
         this.timestamp = timestamp;
         this.lasthash = lasthash;
         this.hash = hash;
-        this.data.data;
+        this.data = data;
     }
 
-    toString(){
+    toString() {
         return `Block =
                 Timestamp = ${this.timestamp}
                 lastHash = ${this.lasthash}
