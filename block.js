16ed74cb62657573b0f72b12b8551422e3194592
cristiano <originalvgo@gmail.com>
2022-08-09 14:19:23 -0300

    src zip

---
+++ block.js
@@ -0,0 +1,17 @@
+class Block {
+
+    constructor(timestamp, lasthash, hash, data){
+        this.timestamp = timestamp;
+        this.lasthash = lasthash;
+        this.hash = hash;
+        this.data.data;
+    }
+
+    toString(){
+        return `Block =
+                Timestamp = ${this.timestamp}
+                lastHash = ${this.lasthash}
+                hash = ${this.hash}
+                data = ${this.data}`;
+    }
+}
