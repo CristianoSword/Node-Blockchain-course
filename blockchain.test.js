b86e0297da8c302eb41194b63de7a194a31855e8
cristiano <originalvgo@gmail.com>
2022-08-10 10:56:13 -0300

    material

---
+++ blockchain.test.js
@@ -0,0 +1,22 @@
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+describe ('Blockchain', () => {
+    let bc;
+    beforeEach(() => {
+        bc = new Blockchain;
+    });
+
+    it('start with genesis block', () => {
+        expect(bc.chain[0]).toEqual(Block.genesis());
+    });
+
+    it('add a new block', () => {
+
+        const data = 'arquivo.pdf';
+        bc.addBlock(data);
+
+        expect(bc.chain[bc.chain.length-1]).data).toEqual(data);
+    });
+}); 
+
