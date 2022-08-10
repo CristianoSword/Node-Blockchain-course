c0febcf866568f50c501f6cccf52bb2e81e7f09b
cristiano <originalvgo@gmail.com>
2022-08-10 10:26:21 -0300

    testes

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
 
     constructor(timestamp, lasthash, hash, data) {
@@ -27,6 +29,11 @@ class Block {
 
         return new this (timestamp, lastHash, hash, data);
     }
+
+    static hash (timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    }
+    // SHA - 256
 }
 
 module.exports = Block;
--- teste.js
+++ teste.js
@@ -1,4 +1,4 @@
-const Block = require('./block.js');
+const Block= require('./block.js');
 
 const block = new Block('666', '666sixsix', 'six666six', '666');
 console.log(block.toString());
